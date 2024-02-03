b952fb6aed1c0232faef1be0a6ecb2072db31b4f
Sorin Guga <sorin.guga@gmail.com>
2024-02-03 21:49:50 +0200

    Adds APP_VERSION, APP_NAME and APP_HUMAN_NAME global variables

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+APP_VERSION = '0.0.1'
+APP_NAME = Rails.application.config.session_options[:key].sub(/^_/,'').sub(/_session/,'')
+APP_HUMAN_NAME = APP_NAME.humanize.split.map(&:capitalize).join(' ')
