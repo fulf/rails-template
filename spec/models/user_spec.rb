325dccf231b06031e0ce8f7d7e25aefef1394140
Sorin Guga <sorin.guga@gmail.com>
2024-02-29 16:25:09 +0200

    More RSpec workflow improvements

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,18 @@
 # typed: false
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 describe 'User' do
   let(:user) { true }
 
   it 'is valid' do
     expect(user).to be false
+  end
+
+  it 'creates something in the db' do
+    expect do
+      PgHero::SpaceStats.create(database: 'foo', schema: 'bar', relation: '', size: 100, captured_at: Time.current)
+    end.to change(PgHero::SpaceStats, :count).by(1)
   end
 end
